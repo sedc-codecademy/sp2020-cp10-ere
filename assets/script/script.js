0cef6086452b66d4e70bd94c6b00ec9da13a9dd5
Petar Gjorgjioski <58377922+LordGrim15@users.noreply.github.com>
2020-07-04 17:39:18 +0200

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -525,7 +525,6 @@ function productSectionElementsListener() {
 	}
 }
 
-
 function productSectionElements(el, e) {
 	if (el.classList.value === `fas fa-times`) {
 		e.stopPropagation();
@@ -538,7 +537,6 @@ function productSectionElements(el, e) {
 			printCardItems();
 			productsInCartNumber();
 		}
-
 	} else if (el.classList[0] === `quantityStyleButtons` || el.parentElement.className === `quantityStyleButtons`) {
 		e.stopPropagation();
 		if (el.parentElement.parentElement.parentElement.attributes['data-text'] === undefined) {
@@ -635,10 +633,10 @@ contact.addEventListener('click', function() {
 });
 
 //CONTACT VALIDATION
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const text = document.getElementById("text");
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const text = document.getElementById('text');
 
 var nameCheck = false;
 var emailCheck = false;
@@ -646,64 +644,61 @@ var phoneCheck = false;
 var textCheck = false;
 
 function ValidateName(inputText, checker) {
-    var nameformat = /^[a-zA-Z]{3,20}(?: [a-zA-Z]+){0,2}$/;
-    if (inputText.value.match(nameformat)) {
-        checker = true;
-        inputText.style.border = "none";
-    } else {
-        inputText.style.border = "Solid red 1px";
-        checker = false;
-    }
+	var nameformat = /^[a-zA-Z]{3,20}(?: [a-zA-Z]+){0,2}$/;
+	if (inputText.value.match(nameformat)) {
+		checker = true;
+		inputText.style.border = 'none';
+	} else {
+		inputText.style.border = 'Solid red 1px';
+		checker = false;
+	}
 }
 
 function ValidateEmail(inputText, checker) {
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (inputText.value.match(mailformat)) {
-        checker = true;
-        inputText.style.border = "none";
-
-    } else {
-        inputText.style.border = "Solid red 1px";
-        checker = false;
-    }
+	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if (inputText.value.match(mailformat)) {
+		checker = true;
+		inputText.style.border = 'none';
+	} else {
+		inputText.style.border = 'Solid red 1px';
+		checker = false;
+	}
 }
 
 function ValidatePhoneNumber(inputText, checker) {
-    if (inputText.value.trim().length > 15) {
-        inputText.style.border = "Solid red 1px";
-        checker = false;
-    } else {
-        inputText.style.border = "none";
-        checker = true;
-    }
+	if (inputText.value.trim().length > 15) {
+		inputText.style.border = 'Solid red 1px';
+		checker = false;
+	} else {
+		inputText.style.border = 'none';
+		checker = true;
+	}
 }
 
 function ValidateText(inputText, checker) {
-    if (inputText.value.length < 10 || inputText.value.length > 300) {
-        inputText.style.border = "Solid red 1px";
-        checker = false;
-    } else {
-        inputText.style.border = "none";
-        checker = true;
-    }
+	if (inputText.value.length < 10 || inputText.value.length > 300) {
+		inputText.style.border = 'Solid red 1px';
+		checker = false;
+	} else {
+		inputText.style.border = 'none';
+		checker = true;
+	}
 }
 
-
-name.addEventListener("focusout", function() {
-    ValidateName(name, nameCheck);
+name.addEventListener('focusout', function() {
+	ValidateName(name, nameCheck);
 });
 
-email.addEventListener("focusout", function() {
-    ValidateEmail(email, emailCheck);
+email.addEventListener('focusout', function() {
+	ValidateEmail(email, emailCheck);
 });
 
-phone.addEventListener("focusout", function() {
-    ValidatePhoneNumber(phone, phoneCheck)
+phone.addEventListener('focusout', function() {
+	ValidatePhoneNumber(phone, phoneCheck);
 });
 
-
-text.addEventListener("focusout", function() {
-    ValidateText(text, textCheck)
+text.addEventListener('focusout', function() {
+	ValidateText(text, textCheck);
 });
 
 // KOGA KE OTIDES VO DRINKS I STAVIS NEKOJ DRINK VO KOSNICKA PUKA KODOT!
